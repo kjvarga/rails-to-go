efebd5f1eb11de2c3bc9736873a1fc401161e660
Karl Varga <kjvarga@gmail.com>
2009-12-24 17:22:33 +0800

    Remove migrations that aren't needed.

--- db/migrate/20090925080529_alter_posts_content_to_text.rb
+++
@@ -1,9 +0,0 @@
-class AlterPostsContentToText < ActiveRecord::Migration
-  def self.up
-    change_column :posts, :content, :text
-  end
-
-  def self.down
-    change_column :posts, :content, :string
-  end
-end
