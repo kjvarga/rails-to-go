d7abfcce011c07023aca8c0472aaeef91063de69
Karl Varga <kjvarga@gmail.com>
2009-12-24 17:31:18 +0800

    Remove all views, controllers, models etc that we don't need.

--- app/controllers/scaffold/base_controller.rb
+++
@@ -1,9 +0,0 @@
-class Scaffold::BaseController < ApplicationController
-  #before_filter :require_user
-
-  ActiveScaffold.set_defaults do |config| 
-    config.ignore_columns.add [:created_at, :updated_at]
-    config.list.per_page = 50
-    config.actions << :nested
-  end
-end
