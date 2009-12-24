88f0867c44bea711b9052c0fcabdc460797b1432
Karl Varga <kjvarga@gmail.com>
2009-12-24 17:09:58 +0800

    Remove vendored gems, update to 2.3.5 and configure for GemCutter gems

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.4' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -12,22 +12,13 @@ Rails::Initializer.run do |config|
   # config.load_paths += %W( #{RAILS_ROOT}/extras )
 
   config.gem "haml"
-  config.gem 'chriseppstein-compass', :lib => 'compass', :source => 'http://gems.github.com'
-  config.gem "mislav-will_paginate", :lib => 'will_paginate', :source => 'http://gems.github.com'
+  config.gem 'compass'
+  config.gem 'will_paginate'
   config.gem 'authlogic'
   config.gem 'searchlogic'
-  config.gem "mbleigh-acts-as-taggable-on", :source => "http://gems.github.com", :lib => "acts-as-taggable-on"
-  config.gem 'machinist',   :lib => false
-  
-  # cron tasks
-  config.gem 'javan-whenever', :lib => false, :source => 'http://gems.github.com'
-
-  # comments
-  config.gem 'jackdempsey-acts_as_commentable', :lib => 'acts_as_commentable', :source => "http://gems.github.com"
-  
-  # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
-  # config.frameworks -= [ :active_record, :active_resource, :action_mailer ]
-  # config.active_record.observers = :cacher, :garbage_collector, :forum_observer
+  config.gem 'acts-as-taggable-on'
+  config.gem 'machinist'
+  config.gem 'acts_as_commentable'
 
   config.time_zone = 'UTC'
 end
